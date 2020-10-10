e9f0bdf64c23d9cb55e0d81505e8bb6f8818d3e6
0sorojo <redrootproductions@gmail.com>
2020-10-09 20:13:17 -0400

    dashboard linked and styled ordered and colored

--- src/Views/Components/Dashboard-Card/dashboardCard.jsx
+++ src/Views/Components/Dashboard-Card/dashboardCard.jsx
@@ -6,9 +6,9 @@ const DashboardCard = (props) => {
   const {level, color} = PriorityController(props.priority);
   
   return (
-    <div className="dashboard-card" style={{color:color}}>
-      <h2>Total: {level} </h2>
-      <p>{props.count}</p>
+    <div className="dashboard-card" onClick={props.clicked}style={{color:color}}>
+      <h2 className="header">{level} </h2>
+      <p className="amount">Total: {props.count}</p>
     </div>
   )
 }
